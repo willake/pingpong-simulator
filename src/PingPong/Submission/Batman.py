dae8c1630d44aac70477c9bd6b6df4f8c5662bcb
Lincoln <willakelin@seekrtech.com>
2022-09-30 00:39:37 +0200

    update: add comment to explain

--- src/PingPong/Submission/Batman.py
+++ src/PingPong/Submission/Batman.py
@@ -38,22 +38,56 @@ def make_arm() -> Arm:
 
 # Exercise 2
 def detectCollision(snap1: Snapshot, snap2: Snapshot) -> Optional[Second]:
-    ax= snap1.ball.x
-    ay= snap1.ball.y
-    bx= snap2.ball.x - snap1.ball.x
-    by= snap2.ball.y - snap1.ball.y
-    cx= snap1.segment.p.x
-    cy= snap1.segment.p.y
-    dx= snap2.segment.p.x - snap1.segment.p.x
-    dy= snap2.segment.p.y - snap1.segment.p.y
-    ex= snap1.segment.q.x
-    ey= snap1.segment.q.y
-    fx= snap2.segment.q.x - snap1.segment.q.x
-    fy= snap2.segment.q.y - snap1.segment.q.y
+    # b1 means ball in snap1, b2 means ball in snap2
+    # a = b1, b = b2 - b1
+    # c = p1, d = p2 - p1
+    # e = q1, f = q2 - q1
+    #
+    # In here, B means ball as Pnt, P means p of segement, Q means q of segment 
+    # if B lies on the linear function of segment then
+    # the cross product of BP and BQ should be 0
+    # BP x BQ = 0
+    #
+    # b1 means ball in snap1, b2 means ball in snap2
+    # t is a rate between 0 and 1 in total time (t2 - t1)
+    # Bx = b1x + ((b2x - b1x) * t)
+    # By = b1y + ((b2y - b1y) * t)
+    # ...
+    # In the following code we replace b1 as a and (b2 - b1) as b, and so on
+    # then we calculate BP and BQ
+    # BPx = (cx + (dx * t)) - (ax + (bx * t))
+    # BPy = (cy + (dy * t)) - (ay + (by * t))
+    # BQx = (ex + (fx * t)) - (ax + (bx * t))
+    # BQy = (ey + (fy * t)) - (ay + (by * t))
+    # then do cross product as
+    # (BPx * BQy) - (BPy * BQx) = 0
+    # 
+    # finally we got a quadratic equation
+    # A * t^2 + B * t + C = 0
+    # A = (dx*fy) - (bx*fy) - (dx*by) - (dy*fx) + (by*fx) + (dy*bx)
+    # B = (dx*ey) + (cx*fy) - (bx*ey) - (ax*fy) - (dx*ay) - (cx*by)
+    # - (dy*ex) - (cy*fx) + (by*ex) + (ay*fx) + (dy*ax) + (cy*bx)
+    # C = (cx*ey) - (ax*ey) - (cx*ay) - (cy*ex) + (ay*ex) + (cy*ax)
+    # and we could get two answer of t, and t should between 0 and 1
+    # since we have a probably answer, we could assert whether B is between PQ
+    # the equation is distance(BP) + distance(BQ) = distance(PQ)
+    
+    ax = snap1.ball.x
+    ay = snap1.ball.y
+    bx = snap2.ball.x - snap1.ball.x
+    by = snap2.ball.y - snap1.ball.y
+    cx = snap1.segment.p.x
+    cy = snap1.segment.p.y
+    dx = snap2.segment.p.x - snap1.segment.p.x
+    dy = snap2.segment.p.y - snap1.segment.p.y
+    ex = snap1.segment.q.x
+    ey = snap1.segment.q.y
+    fx = snap2.segment.q.x - snap1.segment.q.x
+    fy = snap2.segment.q.y - snap1.segment.q.y
 
-    A= dx*fy-bx*fy-dx*by-dy*fx+by*fx+dy*bx
-    B= dx*ey+cx*fy-bx*ey-ax*fy-dx*ay-cx*by-dy*ex-cy*fx+by*ex+ay*fx+dy*ax+cy*bx
-    C= cx*ey-ax*ey-cx*ay-cy*ex+ay*ex+cy*ax
+    A = dx*fy-bx*fy-dx*by-dy*fx+by*fx+dy*bx
+    B = dx*ey+cx*fy-bx*ey-ax*fy-dx*ay-cx*by-dy*ex-cy*fx+by*ex+ay*fx+dy*ax+cy*bx
+    C = cx*ey-ax*ey-cx*ay-cy*ex+ay*ex+cy*ax
 
     if not A < 0 and not A > 0 :
         if not B < 0 and not B > 0:
@@ -65,9 +99,9 @@ def detectCollision(snap1: Snapshot, snap2: Snapshot) -> Optional[Second]:
             else:
                 return None
 
-    d=(B*B)-(4*A*C)
-    t1=(-B+math.sqrt(d))/(2*A)
-    t2=(-B-math.sqrt(d))/(2*A)
+    d = (B*B)-(4*A*C)
+    t1 = (-B+math.sqrt(d))/(2*A)
+    t2 = (-B-math.sqrt(d))/(2*A)
 
     if not t1<0 and not t1>1 :
         return Second((snap1.time+(snap2.time-snap1.time)*(t1)))
@@ -75,20 +109,6 @@ def detectCollision(snap1: Snapshot, snap2: Snapshot) -> Optional[Second]:
         return Second((snap1.time+(snap2.time-snap1.time)*(t2)))
     else :
         return None
-
-def pntToD3(pnt: Pnt):
-    return np.array([pnt.x, pnt.y, 0])
-
-def vecToD3(vec: Vec):
-    return np.array([vec.a, vec.b, 0])
-
-# will return radians
-def getSegmentRotation(segment1: Seg, segment2: Seg) -> float:
-    vector1 = segment1.q - segment1.p
-    vector2 = segment2.q - segment2.p
-    unitVector1 = vector1 / np.linalg.norm(vector1)
-    unitVector2 = vector2 / np.linalg.norm(vector2)
-    return np.arccos(np.dot(unitVector1, unitVector2)) 
 
 # Exercise 3
 
